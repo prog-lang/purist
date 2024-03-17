b8b83b190eeff39037d4cf3755e705665fe92c47
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2024-03-18 00:53:37 +0300

    feat(CommonJS): internal representation

---
+++ src/NodeJS/Sacred.hs
@@ -0,0 +1,53 @@
+module NodeJS.Sacred where
+
+-- KEYWORDS
+
+const :: String
+const = "const"
+
+var :: String
+var = "var"
+
+let_ :: String
+let_ = "let"
+
+new :: String
+new = "new"
+
+return :: String
+return = "return"
+
+function :: String
+function = "function"
+
+exports :: String
+exports = "module.exports"
+
+-- SIGNS
+
+assign :: String
+assign = "="
+
+question :: String
+question = "?"
+
+comma :: String
+comma = ","
+
+colon :: String
+colon = ":"
+
+semicolon :: String
+semicolon = ";"
+
+lbrace :: String
+lbrace = "("
+
+rbrace :: String
+rbrace = ")"
+
+lbracket :: String
+lbracket = "{"
+
+rbracket :: String
+rbracket = "}"
